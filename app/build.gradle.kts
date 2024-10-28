92847b06adc4a94a1bad939f13620224e7e11f05
javiersearight <javier.searight@gmail.com>
2024-10-28 10:53:37 -0500

    removed references to kapt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.google.devtools.ksp)
-//    alias(libs.plugins.jetbrains.kotlin.kapt)
     alias(libs.plugins.hilt.kotlin.android)
 }
 
